add36800a70c935771dfc34b78195a65fb32429a
Shad Reza <shadreza100@gmail.com>
2025-07-05 20:43:48 +0600

    feat: Looked at simple values

---
+++ learn/2_values/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// int
+	fmt.Println(1)
+	fmt.Println(1 + 1)
+	fmt.Println(10 % 2)
+
+	// string
+	fmt.Println("1" + "1")
+	fmt.Println("hello from main func")
+
+	// bool
+	fmt.Println(true)
+	fmt.Println(false)
+
+	// floats
+	fmt.Println(10.5)
+	fmt.Println(10.5 + 2)
+	fmt.Println(10.5 - 2)
+	fmt.Println(10.5 * 2)
+	fmt.Println(10.5 / 2)
+}
